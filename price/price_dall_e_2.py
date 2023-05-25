489b72dd8b61fd80b02bae14fa81e5e93d6f186f
marticztn <46967859+marticztn@users.noreply.github.com>
2023-05-25 05:00:52 -0400

    Added dall e price

---
+++ price/price_dall_e_2.py
@@ -0,0 +1,9 @@
+# OpenAI官方价格: https://openai.com/pricing#language-models
+# 1024 × 1024 - $0.020 / image
+# 512 x 512 - $0.018 / image (服务器默认图片大小)
+# 256 x 256 - $ 0.016 / image
+
+IMG_SIZE_512_PRICE = 0.018
+SERVICE_PRICE = 0.05   # Discord server fee (Added by me)
+
+# TOTAL: 0.068 / image
