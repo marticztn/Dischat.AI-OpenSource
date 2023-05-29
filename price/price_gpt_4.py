0cfa26048aa2818562e9ba73ac9189e817cec302
marticztn <46967859+marticztn@users.noreply.github.com>
2023-05-29 06:36:58 -0400

    Rewrote & Improved price calculation accuracy

--- price/price_gpt_4.py
+++ price/price_gpt_4.py
@@ -1,30 +1,28 @@
-# Documentation: https://learn.microsoft.com/en-us/azure/cognitive-services/openai/how-to/chatgpt?pivots=programming-language-chat-completions#managing-conversations
-
 import tiktoken
 
-MODEL = 'gpt-4-0314'
-PROMPT_PRICE = 0.03     # 0.03 / 1k tokens -> Input price
-CMPL_PRICE = 0.06       # 0.06 / 1k tokens -> Output price
-SERVICE_PRICE = 0.05    # Discord server fee (Added by me)
-encoding = tiktoken.encoding_for_model(MODEL)
+# OpenAI Official Pricing Guide: https://openai.com/pricing
+# Documentation: https://learn.microsoft.com/en-us/azure/cognitive-services/openai/how-to/chatgpt?pivots=programming-language-chat-completions#managing-conversations
 
-def getTokenSizeAndPriceFromMsg(input: str, output: str) -> tuple:
-    global encoding
+MODEL_GPT_35 = "gpt-3.5-turbo"
+GPT_35_PRICE = 0.002            # 0.002 / 1k tokens -> Input tokens + Output tokens price
 
-    # Every message has a 'role' and a 'content' field, which is 4 tokens
-    total_tokens = 4
+MODEL_GPT_4 = "gpt-4"
+GPT_4_PMPT_PRICE = 0.03         # 0.03 / 1k tokens -> Input price
+GPT_4_CMPL_PRICE = 0.06         # 0.06 / 1k tokens -> Output price
 
-    # Calculate the number of tokens used by the prompt and the reply from ChatGPT
-    input_tokens = len(encoding.encode(input))
-    output_tokens = len(encoding.encode(output))
-    total_tokens += input_tokens + output_tokens
-    
-    # Every API response(output / reply) is primed with an 'assistant' field behind the scenes
-    total_tokens += 2
+SERVICE_PRICE = 0.03            # Discord server fee (Added by me)
 
-    # Calculate the price for each conversation
-    input_price = PROMPT_PRICE * (input_tokens / 1000)
-    output_price = CMPL_PRICE * (output_tokens / 1000)
-    total_price = input_price + output_price + SERVICE_PRICE
+def getTokenSizeFromMsgs(msgs: list, model = MODEL_GPT_4) -> int:
+    # Use GPT-4 encoding
+    token_size = 0
+    encoding = tiktoken.encoding_for_model(MODEL_GPT_4)
 
-    return (total_tokens, total_price)
+    for msg in msgs:
+        # Every message has a 'role' and a 'content' field, which is 4 tokens
+        token_size += 4
+        for key, val in msg.items():
+            token_size += len(encoding.encode(val))
+
+    # Every API response(output / sample / reply) is primed with an 'assistant' field behind the scenes
+    token_size += 2
+    return token_size
