e8217683f10084d506ff57ff44d53fee14a039f9
marticztn <46967859+marticztn@users.noreply.github.com>
2023-11-22 23:15:52 +0800

    Lowered dall-e price

--- price/price_dall_e_2.py
+++ price/price_dall_e_2.py
@@ -4,5 +4,5 @@
 # 256 x 256 - $ 0.016 / image
 
 IMG_SIZE_512_PRICE = 0.018
-SERVICE_PRICE = 0.05   # Discord server fee (Added by me)
-TOTAL_PRICE = IMG_SIZE_512_PRICE + SERVICE_PRICE    # TOTAL: 0.068 / image
+SERVICE_PRICE = 0.03   # Discord server fee (Added by me)
+TOTAL_PRICE = IMG_SIZE_512_PRICE + SERVICE_PRICE    # TOTAL: 0.048 / image
