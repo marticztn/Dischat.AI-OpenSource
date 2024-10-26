798b7c544c3a1ee3b484ec04cfac48f8c495ed35
mārt1cz <46967859+marticztn@users.noreply.github.com>
2024-10-26 00:11:41 -0500

    Update price_dall_e_3.py

--- price/price_dall_e_2.py
+++ price/price_dall_e_2.py
@@ -1,8 +1,6 @@
-# OpenAI官方价格: https://openai.com/pricing#language-models
-# 1024 × 1024 - $0.020 / image
-# 512 x 512 - $0.018 / image (服务器默认图片大小)
-# 256 x 256 - $ 0.016 / image
+# OpenAI official pricing: https://openai.com/api/pricing/
+# 1024 × 1024 - $0.040 / image
 
-IMG_SIZE_512_PRICE = 0.018
-SERVICE_PRICE = 0.03   # Discord server fee (Added by me)
-TOTAL_PRICE = IMG_SIZE_512_PRICE + SERVICE_PRICE    # TOTAL: 0.048 / image
+IMG_SIZE_1024_PRICE = 0.040
+SERVICE_PRICE = 0.005   # Discord server fee (Added by me) TOTAL: 0.045 / image
+FILENAME = os.path.dirname(__file__) + '/../users.json'
